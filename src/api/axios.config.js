7ff52b6b8f0c318159fa4a487339f4d93a4905d8
PredatorR10 <godofgamingpr10@gmail.com>
2022-04-30 19:32:40 -0400

    added missing quotes

--- src/api/axios.config.js
+++ src/api/axios.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //API = "http://127.0.0.1:8000/api"
-API = REACT_APP_API_URL
+API = "https://workoutjournal-backend.herokuapp.com/api/"
 
 const tellWorkoutTo = axios.create({
     baseURL: API,
