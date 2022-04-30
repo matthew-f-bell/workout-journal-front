eafa4d1840fc136b1c5c9a01fa466c25379a4269
PredatorR10 <godofgamingpr10@gmail.com>
2022-04-30 19:36:08 -0400

    fixed a variable declaration typo

--- src/api/axios.config.js
+++ src/api/axios.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//API = "http://127.0.0.1:8000/api"
-API = "https://workoutjournal-backend.herokuapp.com/api/"
+//const API = "http://127.0.0.1:8000/api"
+const API = "https://workoutjournal-backend.herokuapp.com/api/"
 
 const tellWorkoutTo = axios.create({
     baseURL: API,
