109a33a9c7fa42833036e73582c576e0e04bcd86
xdjx3runx <xdjx3runx@gmail.com>
2022-04-30 17:48:17 -0700

    fixed merge error

--- src/api/axios.config.js
+++ src/api/axios.config.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-<<<<<<< HEAD
-const API = "http://127.0.0.1:8000/api"
-//const API = "https://workoutjournal-backend.herokuapp.com/api/"
-=======
 const DEPLOYMENT = false
 let API;
 
@@ -14,7 +10,6 @@ if(DEPLOYMENT) {
 }
 //
 
->>>>>>> 6a64c047aa7ed5aa76bbe6f8cc8e2cd3e6afd157
 
 const tellWorkoutTo = axios.create({
     baseURL: API,
